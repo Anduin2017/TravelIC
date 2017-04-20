5a3280fc4c733a82ab38bba7698aad3e103deec3
bianxiangg <mymailbox001@163.com>
2017-04-20 15:10:52 +0800

    clear secrets.cs

--- TravelInCloud/Secrets.cs
+++ TravelInCloud/Secrets.cs
@@ -7,16 +7,16 @@ namespace TravelInCloud
 {
     public static class Secrets
     {
-        public static string AppID { get; set; } = "wxe826ff293d23c73c";
-        public static string AppSecret { get; set; } = "3b823f2c6e5b99da959af945fcf0afa3";
-        public static string Token { get; set; } = "TTWEKHDLOIHJO";
-        public static string Host { get; set; } = "travelincloud.obisoft.com.cn";
-        public static string TempUserName { get; set; } = "@tp.obisoft.com.cn";
-        public static string TempPassword { get; set; } = "TI_f83-J[)1";
-        public static string mch_id { get; set; } = "1401038702";
-        public static string device_info { get; set; } = "WEB";
-        public static string NOTIFY_URL { get; set; } = "https://travelincloud.obisoft.com.cn/home/ResultNotifyPage";
-        public static string IP { get; set; } = "8.8.8.8";
-        public static string KEY { get; set; } = "yuehepingyueheping18511177788yhp";
+        public static string AppID { get; set; } = "";
+        public static string AppSecret { get; set; } = "";
+        public static string Token { get; set; } = "";
+        public static string Host { get; set; } = "";
+        public static string TempUserName { get; set; } = "";
+        public static string TempPassword { get; set; } = "";
+        public static string mch_id { get; set; } = "";
+        public static string device_info { get; set; } = "";
+        public static string NOTIFY_URL { get; set; } = "";
+        public static string IP { get; set; } = "";
+        public static string KEY { get; set; } = "";
     }
 }
